fb88cf69b6b1084a1c6ea6b221a2ee7fdfab7dae
jeffreybiles <bilesjeffrey@gmail.com>
2012-11-09 20:05:48 -0500

    canvas with background-color styling

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ app.configure(function(){
   app.use(express.bodyParser());
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
+  app.use(express.static(__dirname + '/public'));
 });
 
 
