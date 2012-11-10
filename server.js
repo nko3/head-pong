fb234a81499e6722ffd0331f655f45a5942865e0
Oscar Rendon <oarendon@gmail.com>
2012-11-09 19:18:08 -0500

    refactor to express, added ignore file.

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
-var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
-}).listen(8000);
 
-console.log('Server running at http://0.0.0.0:8000/');
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res){
+  res.send('Hello World');
+});
+
+app.listen(3000);
+console.log('Listening on port 3000');
+
